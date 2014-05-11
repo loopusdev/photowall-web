44d206764bd681288421cef2243d2e6eabdff1cc
Martin Sosic <sosic.martin@gmail.com>
2014-05-11 08:19:24 +0200

    Added photo wall animation

--- app/scripts/controllers/wall.js
+++ app/scripts/controllers/wall.js
@@ -7,12 +7,15 @@ angular.module('photowallWebApp')
     '$routeParams',
     'services.rest',
     'services.socket',
-    function ($scope, $routeParams, rest, socket) {
+    '$timeout',
+    function ($scope, $routeParams, rest, socket, $timeout) {
 
       // Fetch photos - first 4?
       $scope.wallID   = undefined;
       $scope.wallName = $routeParams.wallName;
-      $scope.photos   = [];
+      $scope.activePhotos = []; // Photos that are shown on wall
+      $scope.inactivePhotos = []; // Photos not shown on wall
+      var numMaxActivePhotos = 40; // Maximal number of active photos
 
 
       rest.getWall($scope.wallName,
@@ -30,7 +33,9 @@ angular.module('photowallWebApp')
       rest.getPhotos($scope.wallName, 
         // Callback
         function(photosObject) {
-          $scope.photos = photosObject.data;
+          var photos = photosObject.data;
+          $scope.activePhotos =   photos.slice(0, numMaxActivePhotos);
+          $scope.inactivePhotos = photos.slice(numMaxActivePhotos);
         },
         // Error
         function(data) {
@@ -40,17 +45,32 @@ angular.module('photowallWebApp')
 
       socket.registerListener(function(msg) {
         $scope.$apply(function() {
-
           // Check id - if matches, push to photos array
-          if (msg.data.wallId === $scope.wallID)
-            $scope.photos.unshift(msg.data);          
+          if (msg.data.wallId === $scope.wallID) {
+            // move one from active to inactive photos and push new to active
+            $scope.inactivePhotos.push($scope.activePhotos[0]);
+            $scope.activePhotos[0] = msg.data;
+          }    
 
         });
       });
 
-      /*
-      // Final resulst
-      $scope.photosToShow = undefined;
-      */
+      var getRandom = function(upperBound) {
+        return Math.floor(Math.random() * upperBound);
+      };
+
+      // Each 3 seconds, swap one photo from inactive photos with one from active photos
+      var swapRandomPhoto = function() {
+        var idActive = getRandom($scope.activePhotos.length - 1) + 1;
+        var idInactive = getRandom($scope.inactivePhotos.length);
+        var tmp = $scope.inactivePhotos[idInactive];
+        $scope.inactivePhotos[idInactive] = $scope.activePhotos[idActive];
+        $scope.activePhotos[idActive] = tmp;
+        $timeout(function() {
+          swapRandomPhoto();
+        }, 3000);
+      };
+
+      swapRandomPhoto();
 
 }]);
